756dc7c71ddd8538b9bb2d1597175020bd35a994
Kiran Shahi <mail@kirans.com.np>
2017-01-17 23:57:22 +0545

    List all user for admin has been added.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   
+  def index
+
+    if current_user.admin?
+        @users = User.all 
+    else
+        redirect_to root_path
+    end  
+  
+  end
+
   def new
 
     if current_user.admin?
